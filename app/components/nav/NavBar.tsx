7a84d6239d7712e3bec846c37346ecdcb1469ef3
Mohamed Hosni <mohamed.hosni9997@gmail.com>
2025-05-08 01:22:36 +0300

    Update NavBar.tsx

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -6,29 +6,26 @@ import { useGetUser } from '@/lib/queryFunctions'
 import User from '../User'
 import SkeletonCustom from '../SkeletonCustom'
 
-
 const NavBar = () => {
-  
   const {user, isLoading} = useGetUser();
 
   return (
-    <nav>
-      <header className="flex justify-between items-center ">
-        <Search/>
-          {isLoading ? (
-          <SkeletonCustom circle/>
-          ) : user?.data ? ( 
+    <nav className="px-4 py-3">
+      <header className="flex justify-between items-center flex-wrap gap-4">
+        <Search />
+        {isLoading ? (
+          <SkeletonCustom circle />
+        ) : user?.data ? ( 
           <User user={user.data} /> 
-          ) : (
-            <div className="flex items-center gap-2">
-              <ButtonGame  link='/login' text='Login'/>
-              <ButtonGame link='/signup' text='Sign up'/>
-            </div>
-          )
-        }
+        ) : (
+          <div className="flex items-center gap-2">
+            <ButtonGame link='/login' text='Login'/>
+            <ButtonGame link='/signup' text='Sign up'/>
+          </div>
+        )}
       </header>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
